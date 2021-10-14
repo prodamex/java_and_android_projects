b9b63ef42e73e3d0811acc44746f4c3f2a72621c
unes <64701810+prodamex@users.noreply.github.com>
2021-10-14 09:48:45 +0200

    Update Bataille.java

--- jeuCarte/src/jeuCarte/Bataille.java
+++ jeuCarte/src/jeuCarte/Bataille.java
@@ -1,65 +1,136 @@
 package jeuCarte;
 
 import java.util.ArrayList;
-import java.util.*;
+
 
 public class Bataille {
 	
 	public static void main(String [] args )
 	{
-		List<Carte> paquetDeCartes = new ArrayList<Carte>();
+		ArrayList<Carte> paquetDeCartes = new ArrayList<Carte>();
+		ArrayList<Carte> paquetDeCartesMelange = new ArrayList<Carte>();
 		ArrayList<Carte> paquetJoueur1 = new ArrayList<Carte>();
 		ArrayList<Carte> paquetJoueur2 = new ArrayList<Carte>();
 		
-		// Création du paquet de cartes
+		// CrÃ©ation du paquet de cartes
 		
 		for(int i=0; i<13; i++)
 		{
 			for(int j=0; j<4; j++)
 			{
 				paquetDeCartes.add(new Carte(i,j));
-				
 			}
-
-		}
-      
-		
-
-		// Création du paquet de cartes du joueur 1
-		
-		for(int i=0; i<26; i++) 
-		{
-			paquetJoueur1.add(paquetDeCartes.get(i)); 
 		}
 		
-		// Création du paquet de cartes du joueur 2
-		
-		for(int i=26; i<52; i++) 
+		while(paquetDeCartes.size()>0)
 		{
-			paquetJoueur2.add(paquetDeCartes.get(i)); 
+			int index = (int)(Math.random()*paquetDeCartes.size());
+			paquetDeCartesMelange.add(paquetDeCartes.remove(index));
+		}
+		
+		// CrÃ©ation du paquet de cartes du joueur 1 et joueur 2
+		
+		for(int i=0; i<52; i++) 
+		{
+			if(i%2==0)
+			{
+				paquetJoueur1.add(paquetDeCartesMelange.get(i)); 
+			}
+			else
+			{
+				paquetJoueur2.add(paquetDeCartesMelange.get(i)); 
+			}
+			
 		}
 		
 		Joueur joueur1 = new Joueur(26, paquetJoueur1);
 		Joueur joueur2 = new Joueur(26, paquetJoueur2);
 		
 		boolean jouer=true;
-		
 	
 		while(jouer)
 		{
 			Carte carte1 = joueur1.tirerCarte();
 			Carte carte2 = joueur2.tirerCarte();
 			
-			if(carte1.compareCarte(carte2))
+			if(joueur1.getCompteurDePoints()==50)
+			{
+				System.out.println("le joueur 1 a gagnÃ©");
+				jouer=false;
+			}
+			else if(joueur2.getCompteurDePoints()==50)
+			{
+				System.out.println("le joueur 2 a gagnÃ©");
+				jouer=false;
+			}
+			
+			if(carte1.compareCarte(carte2)==true)
 			{
 				joueur1.ajouterCarte(carte1);
 				joueur1.ajouterCarte(carte2);
 			}
-			else
+			else if(carte1.compareCarte(carte2)==false)
 			{
 				joueur2.ajouterCarte(carte1);
 				joueur2.ajouterCarte(carte2);
 			}
+			else
+			{
+				System.out.println("**** BATAILLE ****");
+				
+
+				if(joueur1.getCompteurDePoints()==2)
+				{
+					System.out.println("le joueur 1 a gagnÃ©");
+					jouer=false;
+					break;
+				}
+				else if(joueur2.getCompteurDePoints()==2)
+				{
+					System.out.println("le joueur 2 a gagnÃ©");
+					jouer=false;
+					break;
+				}
+				
+				
+				Carte c1 = joueur1.tirerCarte();
+				Carte c2 = joueur2.tirerCarte();
+				
+				Carte b1 = joueur1.tirerCarte();
+				Carte b2 = joueur2.tirerCarte();
+				
+			
+				if(b1.compareCarte(b2)==true)
+				{
+					joueur1.ajouterCarte(carte1);
+					joueur1.ajouterCarte(carte2);
+					joueur1.ajouterCarte(c1);
+					joueur1.ajouterCarte(c2);
+					joueur1.ajouterCarte(b1);
+					joueur1.ajouterCarte(b2);
+				}
+				else if(b1.compareCarte(b2)==false)
+				{
+					joueur2.ajouterCarte(carte1);
+					joueur2.ajouterCarte(carte2);
+					joueur2.ajouterCarte(c1);
+					joueur2.ajouterCarte(c2);
+					joueur2.ajouterCarte(b1);
+					joueur2.ajouterCarte(b2);
+				}
+				else
+				{
+					System.out.println("****** EGAUX ******");
+					joueur1.ajouterCarte(carte1);
+					joueur2.ajouterCarte(carte2);
+					joueur1.ajouterCarte(c1);
+					joueur2.ajouterCarte(c2);
+					joueur1.ajouterCarte(b1);
+					joueur2.ajouterCarte(b2);
+				}
+				
+			}
+			
 			
 
 			System.out.println(" Score du joueur 1 " + joueur1.getCompteurDePoints());
@@ -67,18 +138,6 @@ public class Bataille {
 			
 			System.out.println("*****************************");
 			
-			if(joueur1.getCompteurDePoints()>50)
-			{
-				System.out.println("le joueur 1 a gagné");
-				jouer=false;
-				break;
-			}
-			else if(joueur2.getCompteurDePoints()>50)
-			{
-				System.out.println("le joueur 2 a gagné");
-				jouer=false;
-				break;
-			}
 			
 
 		}
